b407247a4e410210ec65492606ce9849a0df4115
pehamTom <pehamtom@gmx.at>
2017-05-08 20:16:37 +0200

    asd

--- MMS-Projekt/src/imagingApp/Main.java
+++ MMS-Projekt/src/imagingApp/Main.java
@@ -3,16 +3,7 @@ package imagingApp;
 public class Main {
 
 	public static void main(String[] args) {
-		//comment
-		System.out.println("Test");
-		//change to source code
-		//seperate change
-		//test
-		//haha
-		//Sdfsdf
-		System.out.println("Hello World");
-		//change
-		//comment
+		
 	}
 
 }
